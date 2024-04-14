90fce5e8fb28160bc641748c164ab14cefb9508c
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-04-14 23:04:51 +0530

    constant.ts initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import Navbar from "@/components/global/navbar";
+import { Button } from "@/components/ui/button";
 import { dividerClasses } from "@mui/material";
 import Image from "next/image";
 
@@ -13,7 +14,15 @@ export default function Home() {
           <ContainerScroll
           titleComponent ={
             <div className="flex items-center justify-center flex-col">
-              
+              <Button
+                  size={'lg'}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                ><span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                Start For Free Today
+              </span></Button>
+              <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With Fuzzie
+                </h1>
             </div>
           }
           ></ContainerScroll>
---
+++ src/lib/constant.ts
@@ -0,0 +1,5 @@
+export const clients = [...new Array(10)].map(
+    (client,index)=>({
+        href :  `/${index+1}.png`,
+    })
+)
