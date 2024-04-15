95cf0cf1085949e515f2cd242a7b0fca675e589a
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-04-15 23:27:48 +0530

    sparkles.tsx effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { HeroParallax } from "@/components/global/connect-parallax";
 import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import { InfiniteMovingCards } from "@/components/global/infinite-moving-cards";
+import { LampComponent } from "@/components/global/lamp";
 import Navbar from "@/components/global/navbar";
 import { Button } from "@/components/ui/button";
 import { clients, products } from "@/lib/constant";
@@ -41,7 +42,10 @@ export default function Home() {
         <HeroParallax products={products}></HeroParallax>
       </section>
       <section className="mt-[-500px]">
-        <LampComponent></LampComponent>
+        <LampComponent />
+        <div className="flex flex-wrap items-center justify-center flex-col  md:flex-row gap-8 -mt-72">
+          <CardContainer className='inter-var'></CardContainer>
+        </div>
       </section>
     </main>
   );
