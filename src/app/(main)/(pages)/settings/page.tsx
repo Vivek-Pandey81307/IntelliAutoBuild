43136bc876f278a192aaf62cd83f195d9ec1cdf4
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-06-29 20:36:08 +0530

    uploader install

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -2,23 +2,34 @@ import ProfileForm from '@/components/forms/proflie-form'
 import React from 'react'
 import ProfilePicture from './_components/profile-picture'
 import { db } from '@/lib/db'
+import { currentUser } from '@clerk/nextjs/server'
 
 type Props = {}
 
-const Settings = (props: Props) => {
-  // const removeProfileImage = async ()=>{
-  //   'use server'
-  //   const response = await db.user.update({
-  //     where : {
-  //       clerkId : authUser.id,
-  //     }
-  //     data : {
-  //       profileImage : '',
+const Settings = async(props: Props) => {
+  const authUser = await currentUser()
+  if(!authUser) return null
+  const user = await db.user.findUnique({where : {clerkId : authUser.id }})
+  const removeProfileImage = async ()=>{
+    'use server'
+    const response = await db.user.update({
+      where : {
+        clerkId : authUser.id,
+      },
+      data : {
+        profileImage : '',
       
-  //     },
-  //   })
-  //   return response
-  // }
+      },
+    })
+    return response
+  }
+  const uploadProfileImage = async (image : string) =>{
+    'use server'
+    const response = await db.user.update({
+      where :{ clerkId  : authUser.id},
+      data :{profileImage : image},
+    })
+  }
   return (
     <div className='flex flex-col gap-4'>
         <h1 className='sticky top-0 z-[10] flex items-center
@@ -31,11 +42,11 @@ const Settings = (props: Props) => {
                 Add or update your information
             </p>
         </div>
-        {/* <ProfilePicture
+        <ProfilePicture
         onDelete={removeProfileImage}
         userImage={user?.profileImage || ''}
         onUpload={uploadProfileImage}
-        ></ProfilePicture> */}
+        />
         <ProfileForm />
     </div>
   )
