4add60bfca11a24d1a69169b38e04f8433f7aa39
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-07-05 11:15:00 +0530

    UploadCare Error fixed

--- src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -2,23 +2,24 @@
 import React, { useState } from 'react';
 import { FileUploaderRegular } from '@uploadcare/react-uploader';
 import '@uploadcare/react-uploader/core.css';
+import { boolean } from 'zod';
 
 function UploadCareButton() {
   const [files, setFiles] = useState([]);
-  
+  let b =1;
   const handleChangeEvent = (items) => {
-      setFiles([...items.allEntries.filter((file) => file.status === 'success')]);
+      setFiles([...items.allEntries.filter((file) => file.status === 'success')]);b=0;
     };
 
   return (
     <div>
-      <FileUploaderRegular onChange={handleChangeEvent} pubkey="ed4090b17a4d0cfb429a" />
+      {b && <FileUploaderRegular onChange={handleChangeEvent} pubkey="ed4090b17a4d0cfb429a" />}
 
       <div>
         {files.map((file) => (
           <div key={file.uuid}>
             <img
-              src={file.cdnUrl}
+              src={file.cdnUrl + '/-/smart_resize/300x300/-/border_radius/50p/-/setfill/000000/'}
               alt={file.fileInfo.originalFilename}
             />
           </div>
