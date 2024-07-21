0501d1b62355d3d461b88fd8e2427ceda9f57694
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-07-21 14:40:10 +0530

    Configuring Editor Reducer

--- src/providers/editor-provider.tsx
+++ src/providers/editor-provider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { EditorNodeType } from '@/lib/types';
+import { EditorActions, EditorNodeType } from '@/lib/types';
 import {
     Dispatch,
     createContext,
@@ -12,49 +12,93 @@ import {
 export type EditorNode = EditorNodeType;
 
 export type Editor = {
-    elements : EditorNode[];
-    edges : {
-        id : string;
-        source : string;
-        target : string;
+    elements: EditorNode[];
+    edges: {
+        id: string;
+        source: string;
+        target: string;
     }[];
-    selectedNode : EditorNodeType;
+    selectedNode: EditorNodeType;
 }
 
 export type HistoryState = {
-    history : Editor []
-    currentIndex : number
+    history: Editor[]
+    currentIndex: number
 }
-export type EditorState  = {
-    editor : Editor
-    history : HistoryState
+export type EditorState = {
+    editor: Editor
+    history: HistoryState
 }
-const initialEditorState : EditorState['editor'] = {
-    elements :[],
-    selectedNode : {
-        data : {
-            completed : false,
-            current : false,
-            description : '',
-            metadata : {},
-            title : '',
-            type : 'Trigger',
+const initialEditorState: EditorState['editor'] = {
+    elements: [],
+    selectedNode: {
+        data: {
+            completed: false,
+            current: false,
+            description: '',
+            metadata: {},
+            title: '',
+            type: 'Trigger',
         },
-        id:'',
-        position : {x : 0, y : 0},
-        type : 'Trigger'
+        id: '',
+        position: { x: 0, y: 0 },
+        type: 'Trigger'
 
     },
-    edges : [],
+    edges: [],
 }
-const initialHistoryState : HistoryState = {
-    history : [initialEditorState],
+const initialHistoryState: HistoryState = {
+    history: [initialEditorState],
     currentIndex: 0,
 }
-const initialState : EditorState = {
-    editor : initialEditorState,
-    history : initialHistoryState,
+const initialState: EditorState = {
+    editor: initialEditorState,
+    history: initialHistoryState,
 }
-const editorReducer = (state : EditorState = initialState,action : EditorActions)=>{
-    
+const editorReducer = (state: EditorState = initialState, action: EditorActions): EditorState => {
+    switch (action.type) {
+
+        case 'REDO':
+            if (state.history.currentIndex < state.history.history.length - 1) {
+                const nextIndex = state.history.currentIndex + 1
+                const nextEditorState = { ...state.history.history[nextIndex] }
+                const redoState = {
+                    ...state,
+                    editor: nextEditorState,
+                    history: {
+                        ...state.history,
+                        currentIndex: nextIndex,
+                    },
+                }
+                return redoState
+            }
+            return state
+
+        
+        case 'UNDO' : 
+            if (state.history.currentIndex > 0){
+                const prevIndex  = state.history.currentIndex -1 ;
+                const prevEditorState = {...state.history.history[prevIndex]}
+                const undoState = {
+                    ...state ,
+                    editor : prevEditorState,
+                    history : {
+                        ...state.history,
+                        currentIndex : prevIndex,
+                    }
+                }
+            }
+        
+        case 'LOAD_DATA':
+            return {
+                ...state,
+                editor : {
+                    ...state.editor,
+                    elements  : action.payload.elements || initialEditorState.elements,
+                    edges : action.payload.edges,
+                }
+            }
+        default:
+            return state;
+    }
 }
