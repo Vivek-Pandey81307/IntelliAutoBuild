1ce352fa7d63c0a57e538e793ccfa2a3eab5177f
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-07-25 23:49:27 +0530

    added onDrop useCallback fucntion to editor-canvas.tsx

--- src/app/(main)/(pages)/workflows/editor/[editorid[/_components/editor-canvas.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorid[/_components/editor-canvas.tsx
@@ -2,15 +2,60 @@
 import React, { useMemo } from 'react'
 import { ReactFlow } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { EditorNodeType } from '@/lib/types';
+import { EditorCanvasCardType, EditorNodeType } from '@/lib/types';
 import { useEditor } from '@/providers/editor-provider';
 import EditorCanvasCardSingle from './editor-canvas-card-single';
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+  } from "@/components/ui/resizable"
+import { EditorCanvasDefaultCardTypes } from '@/lib/constant';
+import { toast } from '@/components/ui/use-toast';
+
 type Props = {}
 
 const initialNodes : EditorNodeType[] = []
 const initialEdges : {id : string ;source : string; target : string}[]=[]
 const EditorCanvas = (props: Props) => {
     const {dispatch,state}  = useEditor()
+
+    const onDrop= useCallback(
+        (event : any) =>{
+            event.preventDefault()
+            const type : EditorCanvasCardType['type']  = event.dataTransfer.getData('application/reactflow')
+            if(typeof type === 'undefined' || !type){return}
+            const triggerAlreadyExists = state.editor.elements.find(
+                (node) =>node.type === 'Trigger'
+            )
+            if(type === 'Trigger' && triggerAlreadyExists){
+                toast('Only one trigger can be added to automations at the moment')
+                return
+            }
+            if(!reactFlowInstance) return 
+            const position = reactFlowInstance.screenToFlowPosition({
+                x : event.clientX,
+                y : event.clientY,
+            })
+            const newNode = {
+                id : v4(),
+                type ,
+                position,
+                data : {
+                    title : type,
+                    description : EditorCanvasDefaultCardTypes[type].description,
+                    completed : false,
+                    current : false,
+                    metadata : {},
+                    type : type,
+                }
+            }
+            setNodes((nds)=>nds.concat(newNode))
+        },
+        [reactFlowInstance,state]
+        }
+    )
+
     const nodeTypes = useMemo(
         () =>({
             Action :  EditorCanvasCardSingle,
@@ -28,8 +73,32 @@ const EditorCanvas = (props: Props) => {
         }),[]
     )
     return (
-    <div>EditorCanvas</div>
-  )
+        <ResizablePanelGroup
+         direction="horizontal"
+         className=""
+        >
+            <ResizablePanel defaultSize={70} >
+                <div className='flex h-full items-center justify-center'>
+                    <div
+                    style={{width : '100%',height : '100%',paddingBottom : '70px'}}
+                    className='relative'>
+                        <ReactFlow 
+                        className='w-[300px]'
+                        onDrop={onDrop}
+                        onDragOver={onDragOver}
+                        nodes={state.editor.elements}
+                        edges={edges}
+                        onEdgesChange={onEdgesChange}
+                        onConnect={onConnect}
+                        onInit={setReactFlowInstance}
+                        fitView
+                        onClick={handleClickCanvas}
+                        nodeTypes={nodeTypes}></ReactFlow>
+                    </div>
+                </div>
+            </ResizablePanel>
+        </ResizablePanelGroup>
+    )
 }
 
 export default EditorCanvas
