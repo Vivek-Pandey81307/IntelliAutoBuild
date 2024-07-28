bf6b0f00cad1f76b946c0cc1b627747f348ef791
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-07-28 11:13:09 +0530

    created flow-instance.tsx

--- src/app/(main)/(pages)/workflows/editor/[editorid]/_components/flow-instance.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorid]/_components/flow-instance.tsx
@@ -1,22 +1,42 @@
 import { useNodeConnections } from '@/providers/connections-provider'
+import { on } from 'events'
 import { usePathname } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
+import { onCreateNodesEdges, onFlowPublish } from '../_actions/workflow-connections'
+import { toast } from 'sonner'
+import { Button } from '@/components/ui/button'
 
 type Props = {
     children : React.ReactNode
     edges : any[]
     nodes : any[]
 }
+const onPublishWorkflow = useCallback(async()=>{
+    const response = await onFlowPublish(pathname.split('/').pop()!,true)
+    if(response)toast.message(response)
+})
 
 const FlowInstance = ({children,edges,nodes}: Props) => {
     const pathname = usePathname()
     const [isFlow,setIsFlow] = useState([])
     const {nodeConnection} = useNodeConnections()
+    const onFlowAutomation = useCallback(async()=>{
+        const flow  = await onCreateNodesEdges(
+            pathname.split('/').pop()!,
+            JSON.stringify(nodes),
+            JSON.stringify(edges),
+            JSON.stringify(isFlow)
+        )
+        if(flow) toast.message(flow.message)
+
+    },[nodeConnection])
     return (
     <div className='flex flex-col gap-2'>
         <div className='flex gap-3 p-4'> 
             <Button onClick={onFlowAutomation} disabled={isFlow.length <1}>Save</Button>
+            <Button onClick={onPublishWorkflow} disabled={isFlow.length <1}>Publish</Button>
         </div>
+        {children}
     </div>
   )
 }
