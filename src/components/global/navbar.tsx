0d44c9635248afa24f209eecc30dddfb9dd3cab8
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-07-28 18:23:01 +0530

    Headline for intelliAutoBuild fixed

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -8,7 +8,7 @@ type Props = {}
 
 const Navbar = async(props: Props) => {
   return (
-    <header className='fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between'>
+    <header className='fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between text-white'>
         <aside className='flex items-center gap-[2px] transform scale-75 pl-0'>
             <p className='text-3xl font-bold'>Intelli</p>
 
