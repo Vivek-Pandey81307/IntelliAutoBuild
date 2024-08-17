78d1a96c79adc606ec906250e97522f8be388af6
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-08-17 09:53:55 +0530

    Created discord auth callback function

--- src/app/(main)/(pages)/workflows/_components/workflow.tsx
+++ src/app/(main)/(pages)/workflows/_components/workflow.tsx
@@ -1,65 +1,81 @@
 import React from 'react'
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card"
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Label } from '@/components/ui/label'
-import { Switch } from "@/components/ui/switch"
+import { Switch } from '@/components/ui/switch'
+import { toast } from 'sonner'
+import { onFlowPublish } from '../_actions/workflows-connection'
 
 type Props = {
-    name : string
-    description : string
-    id : string
-    publish : boolean | null
+  name: string
+  description: string
+  id: string
+  publish: boolean | null
 }
 
-const Workflow = ({description,id,name,publish}: Props) => {
-  return <Card className="flex w-full items-center justify-between">
-    <CardHeader className='flex flex-col gap-4'>
+const Workflow = ({ description, id, name, publish }: Props) => {
+  const onPublishFlow = async (event: any) => {
+    const response = await onFlowPublish(
+      id,
+      event.target.ariaChecked === 'false'
+    )
+    if (response) toast.message(response)
+  }
+
+  return (
+    <Card className="flex w-full items-center justify-between">
+      <CardHeader className="flex flex-col gap-4">
         <Link href={`/workflows/editor/${id}`}>
-            <div className='flex flex-row gap-2'>
-                <Image 
-                src="/googleDrive.png"
-                alt='Google Drive'
-                height={30}
-                width={30}
-                className='object-contain'
-                />
-                <Image src="/notion.png"
-                alt='Notion.so'
-                height={30}
-                width={30}
-                className='object-contain'/>
-                <Image 
-                src={"/discord.png"}
-                alt='Discord'
-                height={30}
-                width={30}
-                className='object-contain'
-                />  
-            </div>
-            <div className=''>
-                <CardTitle className='text-lg'>{name}</CardTitle>
-                <CardDescription >{description}</CardDescription>
-            </div>
-        
+          <div className="flex flex-row gap-2">
+            <Image
+              src="/googleDrive.png"
+              alt="Google Drive"
+              height={30}
+              width={30}
+              className="object-contain"
+            />
+            <Image
+              src="/notion.png"
+              alt="Google Drive"
+              height={30}
+              width={30}
+              className="object-contain"
+            />
+            <Image
+              src="/discord.png"
+              alt="Google Drive"
+              height={30}
+              width={30}
+              className="object-contain"
+            />
+          </div>
+          <div className="">
+            <CardTitle className="text-lg">{name}</CardTitle>
+            <CardDescription>{description}</CardDescription>
+          </div>
         </Link>
-    </CardHeader>
-    <div className='flex flex-col items-center gap-2 p-4'>
-        <Label htmlFor="airplane-mode" className='text-muted-foreground' >On</Label>
+      </CardHeader>
+      <div className="flex flex-col items-center gap-2 p-4">
+        <Label
+          htmlFor="airplane-mode"
+          className="text-muted-foreground"
+        >
+          {publish! ? 'On' : 'Off'}
+        </Label>
         <Switch
-        id="airplane-mode"
-        // onClick={onPublishFlow}
-        // defaultChecked={}
+          id="airplane-mode"
+          // onClick={onPublishFlow}
+          defaultChecked={publish!}
         />
-    </div>
-  </Card>
+      </div>
+    </Card>
+  )
 }
 
 export default Workflow
