cf129b33a6792f6e22179b4ab063b4baea0e9d01
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-09-25 11:53:21 +0530

    Deployment Error fix

--- src/app/(main)/(pages)/billing/page.tsx
+++ src/app/(main)/(pages)/billing/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Stripe from 'stripe'
+import { google } from 'googleapis';
+import Stripe from 'stripe';
 import { currentUser } from '@clerk/nextjs/server'
 import { db } from '@/lib/db'
 import BillingDashboard from './_components/billing-dashboard'
@@ -15,7 +16,7 @@ const Billing = async (props: Props) => {
   if (session_id) {
     const stripe = new Stripe(process.env.STRIPE_SECRET!, {
       typescript: true,
-      apiVersion: '2023-10-16',
+      apiVersion: '2024-06-20',
     })
 
     const session = await stripe.checkout.sessions.listLineItems(session_id)
--- src/app/(main)/(pages)/connections/_actions/google-connection.tsx
+++ src/app/(main)/(pages)/connections/_actions/google-connection.tsx
@@ -1,6 +1,6 @@
 'use server'
-import clerk from '@clerk/clerk-sdk-node'
-import { auth } from '@clerk/nextjs'
+import { clerkClient } from "@clerk/clerk-sdk-node";
+import { auth } from '@clerk/nextjs/server';
 import { google } from 'googleapis'
 
 export const getFileMetaData = async () => {
@@ -17,11 +17,12 @@ export const getFileMetaData = async () => {
     return { message: 'User not found' }
   }
 
-  const clerkResponse = await clerk.users.getUserOauthAccessToken(
+  const clerkResponse = await clerkClient.users.getUserOauthAccessToken(
     userId,
     'oauth_google'
   )
 
+  // @ts-ignore
   const accessToken = clerkResponse[0].token
 
   oauth2Client.setCredentials({
